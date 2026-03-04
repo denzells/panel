89b4e6e1e0c170f393580efbf13eec8b04180bba
denzells <denzellbest@gmail.com>
2026-03-04 17:29:20 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -239,7 +239,7 @@ local MaxB = mk("TextButton", {
     ZIndex=8, AutoButtonColor=false, Visible=false,
 }, TBar)
 local MaxIcon = mk("ImageLabel", {
-    Image="rbxassetid://99708815872049",
+    Image="rbxassetid://94257904931438",
     Size=UDim2.new(0,10,0,10), Position=UDim2.new(0.5,-5,0.5,-5),
     BackgroundTransparency=1, ImageColor3=C.GRAY, ZIndex=9,
 }, MaxB)
